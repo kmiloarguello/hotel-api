54bd8a49155b52b92f882de23657fe40406d6ee4
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-11-25 19:42:56 -0500

    Creating server

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hotel API
+
+Node app to get Hotel items using Graphql and Express
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const config = require('./config');
+const app = express();
+
+app.get('/', (req,res) => {
+	res.send({message: 'Hola Camilo'})
+})
+
+app.listen(config.port, () => {
+	console.log('Server running on port ', config.port)
+})
+
